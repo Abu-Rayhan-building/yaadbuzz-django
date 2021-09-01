b3d8588b452edd00852ac3c8f78909abf663aacc
Hamidreza Montaseri <hr.montaseri@gandom.co>
2021-09-02 02:16:32 +0430

    add token auth and drf views

--- yaadbuzz_django/urls.py
+++ yaadbuzz_django/urls.py
@@ -14,9 +14,14 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
+from rest_framework.authtoken import views
+from drf_spectacular.views import SpectacularSwaggerView, SpectacularAPIView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api-auth/', include('rest_framework.urls')),
+    path('api-token-auth/', views.obtain_auth_token),
+    path('api/schema/', SpectacularAPIView.as_view(), name='schema'),
+    path('api/schema/swagger', SpectacularSwaggerView.as_view(url_name='schema'), name='swagger')
 ]
