6982553f2adca490b5fd1bef31142079ada26613
Hamidreza Montaseri <hr.montaseri@gandom.co>
2021-09-25 12:35:30 +0330

    Remove is_verified field from user manager

--- users/managers.py
+++ users/managers.py
@@ -5,7 +5,6 @@ class CustomUserManager(BaseUserManager):
     def create_user(self, email, password=None, **extra_fields):
         if not email:
             raise ValueError("The Email must be set")
-        extra_fields.setdefault("is_verified", False)
         email = self.normalize_email(email)
         user = self.model(email=email, **extra_fields)
         user.set_password(password)
@@ -18,7 +17,6 @@ class CustomUserManager(BaseUserManager):
         user = self.create_user(
             email=email,
             password=password,
-            is_verified=True,
             is_staff=True,
             is_superuser=True,
             is_active=True,
